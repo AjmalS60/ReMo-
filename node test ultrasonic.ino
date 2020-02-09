ed4e5eb299c4490d7302506c5252d0d8c5a4c180
AjmalS60 <60496951+AjmalS60@users.noreply.github.com>
2020-02-09 10:49:06 +0530

    Update and rename node.ino to node test ultrasonic.ino

---
+++ node test ultrasonic.ino
@@ -0,0 +1,15 @@
+#include <HCSR04.h>
+
+UltraSonicDistanceSensor sensor(D5,D6);//Transmit and Recieve:
+int dist=0;
+void setup() {
+  //Initialise:
+Serial.begin(9600);
+}
+void loop() 
+{
+ delay(500);
+ double dist = sensor.measureDistanceCm();//To measure the distance im cm:
+ Serial.print(dist);
+ Serial.println(" cm"); 
+}
